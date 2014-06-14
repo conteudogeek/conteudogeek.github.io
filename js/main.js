0fabaa15d47b46c606d69bb23b5454cc26fced96
Leonel Machava <leonelmachava@gmail.com>
2014-06-14 17:40:12 +0200

    Melhorias na UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$("#menu-btn").click(function() {
+  var isOpen = $("#menu").data("isOpen");
 
+  if (isOpen) {
+    $("#menu").slideUp();
+  } else {
+    $("#menu").slideDown();
+  }
+  isOpen = !isOpen;
+  $("#menu").data("isOpen", isOpen);
+})
