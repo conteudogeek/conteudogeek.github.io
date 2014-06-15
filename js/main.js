853e0e413aec6d0d735d8f5fac17f3d23feb2648
Leonel Machava <leonelmachava@gmail.com>
2014-06-15 14:45:40 +0200

    Melhoria no 'Responsive Design'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-$("#menu-btn").click(function(e) {
+function slider(triggerSelector, menuSelector) {
+  $(triggerSelector).click(function(e) {
+    var isOpen = $(menuSelector).data("isOpen");
+
+    if (isOpen) {
+      $(menuSelector).slideUp();
+    } else {
+      $(menuSelector).slideDown();
+    }
+    isOpen = !isOpen;
+    $(menuSelector).data("isOpen", isOpen);
+
+    return false;
+  });  
+}
+
+slider("#menu-btn", "#menu");
+slider("#social-btn", "#social-menu");
+
+/*$("#menu-btn").click(function(e) {
   var isOpen = $("#menu").data("isOpen");
 
   if (isOpen) {
@@ -8,4 +27,4 @@ $("#menu-btn").click(function(e) {
   }
   isOpen = !isOpen;
   $("#menu").data("isOpen", isOpen);
-});
+});*/
